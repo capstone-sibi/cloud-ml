27f352393616a0e6b0059c4c4fe73f736fbb6546
pandora-1 <michaelkahari.mk@gmail.com>
2023-12-21 19:14:16 +0700

    refactor: comment prediction

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
+class URLInput(BaseModel):
+    url: str
 
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
-
-class URLInput(BaseModel):
-    url: str
+    return {"result":"Hello World!!!"}
 
 @app.post("/")
 async def process_url(url_input: URLInput):
     received_url = url_input.url
-    hasil_predict = get_prediction(received_url, "sibi.h5", "weights.43-1.09.hdf5")
-    hasil_predict = "temp"
+    #hasil_predict = get_prediction(received_url, "sibi.h5", "weights.43-1.09.hdf5")
+    hasil_predict = received_url
     return {"result": f"{hasil_predict}"}
